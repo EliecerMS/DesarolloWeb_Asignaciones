5d6cb8cf778e01141b91bf0d827a05907bfe0a77
EliecerMS <ejmendozasoto@gmail.com>
2024-02-02 15:46:23 -0600

    Creacion de Proyecto
    Commit de creacion de proyecto practica 1

---
+++ Practica1_8_PaginasWeb/src/main/java/com/practica1/Practica18PaginasWebApplication.java
@@ -0,0 +1,13 @@
+package com.practica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica18PaginasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica18PaginasWebApplication.class, args);
+	}
+
+}
